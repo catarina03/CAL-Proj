d00e1728726e271fee0d9c2a17d5d9b3fd0f5fda
luis <up201808912@fe.up.pt>
2020-05-16 11:47:53 +0100

    refactoring of code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <Utils.h>
+//#include <Graph.h>
 
 int main() {
+    GraphViewer *gv = new GraphViewer(600, 600, false);
+    vector<int>tmp;
+    createGraph(gv); //creates window and default option for graph
     std::cout << "Hello world" << std::endl;
-    //showGraph("../Maps/GridGraphs/4x4/nodes.txt", "../Maps/GridGraphs/4x4/edges.txt");
-    showGraph("../Maps/PortugalMaps/Aveiro/nodes_x_y_aveiro.txt", "../Maps/PortugalMaps/Aveiro/edges_aveiro.txt");
+    showGraph(gv,"../Maps/GridGraphs/16x16/nodes.txt", "../Maps/GridGraphs/16x16/edges.txt", &tmp);
+    //showGraph(gv,"../Maps/PortugalMaps/Aveiro/nodes_lat_lon_aveiro.txt", "../Maps/PortugalMaps/Aveiro/edges_aveiro.txt", &tmp);
+    //showGraph(gv,"../Maps/PortugalMaps/Portugal/nodes_lat_lon_portugal.txt", "../Maps/PortugalMaps/Portugal/edges_portugal.txt", &tmp);
+
+    getchar();
 };
