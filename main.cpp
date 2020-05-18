ccdf023a81b8000b912aa63e7f24dba00ccde2e7
catarina03 <up201806610@fe.up.pt>
2020-05-19 00:39:01 +0100

    A* is functional

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <csignal>
-#include "src/Utils.h"
+#include "Utils/Utils.h"
+#include "Utils/Visualization.h"
 #include "src/Graph/Graph.h"
 
 int main() {
     signal(SIGINT, SIG_IGN);  //NEEDED OR ELSE IT CRASHES UBUNTU
 
     std::cout << "Welcome to MeetUpRider!" << std::endl;
-
     Graph<Coordinates> graph = mapParser("../Maps/GridGraphs/16x16/nodes.txt", "../Maps/GridGraphs/16x16/edges.txt");
-    //showGraph(&graph);
+
+    //Dijkstra
+    /*
     graph.dijkstraShortestPathByID(0);
     //vector<Coordinates> path = graph.getPathTo(make_pair(300, 600)); //Works in 4x4 and 8x8
     vector<Coordinates> path = graph.getPathTo(make_pair(333, 222)); //Works in 16x16
     showGraph(&graph, path);
+     */
+
+    //A*
+    //16x16
+    Coordinates orig = make_pair(0, 0);
+    Coordinates dest = make_pair(333, 222);
+    vector<Coordinates> result = graph.AStarShortestPathByInfo(orig, dest);
+    showGraph(&graph, result);
+
     return 0;
 };
